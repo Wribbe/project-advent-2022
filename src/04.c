650563c2b4de0be6012b4dd92357af60391c554c
Stefan Eng <eng_steff@outlook.com>
2022-12-08 09:57:59 +0100

    feat: Arrange and print arrays from test-input

--- src/04.c
+++ src/04.c
@@ -3,9 +3,46 @@
 #include "lib/lib.h"
 
 
+int
+next_range(char ** input, size_t range[4])
+{
+  char * char_p = *input;
+  // Format: N1-N2,N3-N4\n
+  for (int ii=0; ii<4; ii++) {
+    range[ii] = strtoul(char_p, &char_p, 10);
+    char_p++; // Skip intermediate '-' and ','.
+  }
+
+  *input = char_p;
+
+  if (*char_p == '\0') {
+    return 0;
+  }
+
+  return 1;
+}
+
+
+void
+print_range(size_t range[4])
+{
+  printf("%zu", range[0]);
+  for (int ii=1; ii<4; ii++) {
+    printf(", %zu", range[ii]);
+  }
+  printf("%s", "\n");
+}
+
+
 size_t
 first(char * input)
 {
+  size_t range[4] = {0};
+
+  while(next_range(&input, range)) {
+    print_range(range);
+  }
+
   return 0;
 }
 

f22ddc07cb5a8c29290e80a2c79c70da911378ed
Stefan Eng <eng_steff@outlook.com>
2022-12-08 22:44:32 +0100

    feat: Printing stack works, though is assembled upside down

--- src/05.c
+++ src/05.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 #include "lib/lib.h"
@@ -12,7 +13,21 @@
 struct info_input {
   size_t num_columns;
   size_t max_crates_possible;
+  char * start;
   char * instructions;
+};
+
+
+struct column {
+  char * top;
+  size_t height;
+};
+
+
+struct stack {
+  size_t len_highest_column;
+  size_t num_columns;
+  struct column * columns;
 };
 
 
@@ -21,6 +36,7 @@ get_info_input(char * input)
 {
   struct info_input info_input = {0};
   info_input.num_columns = 5;
+  info_input.start = input;
 
   size_t counted_rows = 0;
   size_t line_longest = 0;
@@ -65,18 +81,100 @@ print_info_input(struct info_input * info_input)
 }
 
 
+void
+print_stack(struct stack * stack)
+{
+  size_t current_max = stack->len_highest_column;
+
+  for (;current_max > 0;current_max--) {
+
+    for (size_t ii=0; ii<stack->num_columns; ii++) {
+      struct column * column = &stack->columns[ii];
+      if (column->height < current_max) {
+        printf("%s", "   ");
+      } else {
+        printf("[%c]", *(column->top-(column->height-current_max)));
+      }
+    }
+    printf("%s", "\n");
+  }
+}
+
+void
+stack_add(struct stack * stack, size_t index_column, char box)
+{
+
+  if (box == ' ') {
+    return;
+  }
+
+  struct column * column = &stack->columns[index_column];
+
+  column->top += 1;
+  *column->top = box;
+  column->height += 1;
+
+  if (stack->len_highest_column < column->height) {
+    stack->len_highest_column = column->height;
+  }
+}
+
+
 // =============================================================================
 // Part I
 // =============================================================================
 
 
 void
+execute_instructions(struct info_input * info_input, char * output)
+{
+
+  char column_data[info_input->num_columns*info_input->max_crates_possible];
+  memset(column_data, 0, sizeof(column_data));
+  struct column columns[info_input->num_columns];
+
+  struct stack stack = {
+    .len_highest_column = 0,
+    .num_columns = info_input->num_columns,
+    .columns = columns,
+  };
+
+  for (size_t ii=0; ii<info_input->num_columns; ii++) {
+    stack.columns[ii].top = &column_data[ii*info_input->max_crates_possible];
+    stack.columns[ii].height = 0;
+  }
+
+  // Set-up initial state of the stack.
+  char * char_p = info_input->start+1;
+  size_t current_col = 0;
+  for(;;) {
+
+    stack_add(&stack, current_col, *char_p);
+
+    if (*(char_p+2) == '\n') {
+
+      current_col = 0;
+
+      if (*(char_p+4) == '1') {
+        break;
+      }
+
+    } else {
+      current_col++;
+    }
+
+    char_p += 4;
+  }
+
+  print_stack(&stack);
+}
+
+
+void
 first(char * output, struct info_input * info_input)
 {
   print_info_input(info_input);
-  for (size_t ii=0; ii<info_input->num_columns; ii++) {
-    output[ii] = 'X';
-  }
+  execute_instructions(info_input, output);
 }
 
 

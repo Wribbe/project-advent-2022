33f87d835263d097ca10336857921a2b36567af8
Stefan Eng <eng_steff@outlook.com>
2022-12-12 13:40:28 +0100

    feat: Return correct answers for test inputs

--- src/06.c
+++ src/06.c
@@ -1,28 +1,67 @@
 #include "lib/lib.h"
 
 #include <stdio.h>
+#include <stdbool.h>
 
 
 #define LEN(x) sizeof(x)/sizeof(x[0])
 
 
+bool
+duplicate_chars_in_window(char * start, char * end)
+{
+  for (char * temp_end = end; temp_end > start; temp_end--) {
+    for (char * temp_start = start; temp_start < temp_end; temp_start++) {
+      if (*temp_start == *temp_end) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+
 size_t
 answer_1(const char * input)
 {
+  char * start = (char *)input;
+  char * end = start+4;
+
+  for (;;start++,end++) {
+
+    if (*end == '\0') {
+      break;
+    }
+
+    size_t pos_window_start = start-input;
+    size_t pos_window_end = pos_window_start+4;
+
+    if (duplicate_chars_in_window(start, end)) {
+      printf(
+        "Current window @ [%zu,%zu]: %.4s, contains duplicates, continuing.\n",
+        pos_window_start,
+        pos_window_end,
+        start
+      );
+      continue;
+    }
+    return pos_window_end;
+  }
+
   return 0;
 }
 
 
-int
-main(void)
+void
+test_1(void)
 {
-
   struct test_data {
     const char * input;
     size_t correct_result;
   };
 
   struct test_data test_data[] = {
+    {"mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7},
     {"bvwbjplbgvbhsrlpgdmjqwftvncz", 5},
     {"nppdvjthqldpwncqszvftbrmjlhg", 6},
     {"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10},
@@ -34,4 +73,11 @@ main(void)
     size_t result = answer_1(test_data[ii].input);
     printf("expected: %zu, received: %zu\n", test_data[ii].correct_result, result);
   }
+}
+
+
+int
+main(void)
+{
+  test_1();
 }
